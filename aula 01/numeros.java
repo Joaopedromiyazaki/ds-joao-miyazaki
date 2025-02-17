0642d30708baf44c0e4f6b190f829e1be7d7d7d0
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-02-17 09:18:23 -0300

    Aula sobre estrutura e códigos basicos em Java

---
+++ aula 01/numeros.java
@@ -0,0 +1,8 @@
+public class numeros{
+    public static void main(String[] args) {
+       System.out.println(14 + 69);
+       System.out.println(69 - 33);
+       System.out.println(213 * 3);
+       System.out.println(33 / 2);
+    }
+}
