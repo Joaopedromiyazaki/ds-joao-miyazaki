7d1244ee3ec642291971f8acb59b7409225c014d
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-02-24 09:30:54 -0300

    Aula de Java

---
+++ aula 02/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+        public static void main(String[] args) {
+          String name = "pablo marçal";
+          int n1 , n2 ; 
+          System.out.println(name);
+          n1 = 15;
+          n2 = 23;
+          System.out.println("nao vote" + name);
+          System.out.println(n1 + n2);
+          
+                
+          
+        }
+      }
+      
+ 
