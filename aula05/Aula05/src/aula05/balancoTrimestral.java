93339763f8859ac9e120dfb0f877d7a80e814f86
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-04-28 08:54:02 -0300

    Exercicios sobre java no netbeans

---
+++ aula05/Aula05/src/aula05/balancoTrimestral.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class balancoTrimestral {
+    
+ public static void main(String[] args) {
+     int  gastosJaneiro = 15000;
+     int gastosFevereiro = 23000;
+     int gastosMarco = 17000;
+     int gastosTrimestre = (gastosMarco + gastosJaneiro + gastosFevereiro);
+    
+     System.out.println("o gasto trimestral é de "  + gastosTrimestre);
+}
+}
