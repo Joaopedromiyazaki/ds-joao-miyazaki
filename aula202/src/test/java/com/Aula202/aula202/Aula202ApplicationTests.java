50c8ed33ac87d71df48df5cea3442ddc4c47e1a9
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-08-25 09:28:50 -0300

    Aula sobre intelliJ e Springboot

---
+++ aula202/src/test/java/com/Aula202/aula202/Aula202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aula202.aula202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
